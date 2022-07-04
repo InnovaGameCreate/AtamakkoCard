aa1967e9f5ae126188e1baebb359d5c3c0e596f2
Jumpei Nokami <20nj1005@gmail.com>
2022-07-05 08:03:16 +0900

    PUNのテスト

--- Assets/Scripts/TestScene.cs
+++ Assets/Scripts/TestScene.cs
@@ -7,6 +7,9 @@ public class TestScene : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     private void Start()
     {
+        // プレイヤー自身の名前を"Player"に設定する
+        PhotonNetwork.NickName = "Player";
+        
         // PhotonServiceSettingsの設定内容を使ってマスターサーバへ接続する
         PhotonNetwork.ConnectUsingSettings();
     }
