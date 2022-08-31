fae15acea59e80af3984cccd1f0cc2b475e4722f
Jumpei Nokami <20nj1005@gmail.com>
2022-08-31 15:27:09 +0900

    攻撃の実装

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -10,6 +10,7 @@ namespace Player
     public class PlayerAttack : MonoBehaviour
     {
         private AtamakkoStatus _atamakkoStatus;
+        [SerializeField] private AtamakkoStatus enemyStatus;
         [NonSerialized] public int AttackPlace = 0;
         [SerializeField] private GameObject stage;
         [SerializeField] private AttackButton attackButton;
@@ -30,9 +31,10 @@ namespace Player
                 {
                     if (card.Attack[i] == "〇")
                     {
+                        Debug.Log("攻撃可能場所 " + i);
                         var toPlace = (i + _atamakkoStatus.MyPosition) % 6;
                         var attackArea = Instantiate(attackButton, stage.transform.position, Quaternion.identity, stage.transform);
-                        attackArea.transform.rotation = Quaternion.Euler(0f, 0f, 60 * toPlace);
+                        attackArea.transform.rotation = Quaternion.Euler(0f, 0f, 180 + -60 * toPlace);
                         attackArea.AttackPlace = toPlace;
                         attackArea.PlayerAttack = this;
                     }
@@ -44,7 +46,14 @@ namespace Player
 
         private void Attack(int damage)
         {
-            Debug.Log("攻撃 "+AttackPlace);
+            Debug.Log("自分の位置 " + _atamakkoStatus.MyPosition);
+            Debug.Log("敵の位置 " + enemyStatus.MyPosition);
+            Debug.Log("攻撃位置 " + AttackPlace);
+            if (enemyStatus.MyPosition == AttackPlace)
+            {
+                Debug.Log("ダメージ " + damage);
+                enemyStatus.MyHp.Value -= damage;
+            }
         }
     }
 }
