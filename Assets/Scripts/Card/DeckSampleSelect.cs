85a7ffb4211bc6af4f9c624a691668db9fa0784d
tibigame <ryosukehashimoto@tmail.com>
2022-12-20 22:07:20 +0900

    サンプルデッキの追加及び新カードの追加

---
+++ Assets/Scripts/Card/DeckSampleSelect.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Card;
+
+public class DeckSampleSelect : MonoBehaviour
+{
+    [SerializeField]
+    Deck[] decks;
+    [SerializeField]
+    private Image[] ButtonImage;
+
+    private void Start()
+    {
+        ButtonImage[0].color = Color.gray;//選択中のデッキのボタンの色を灰色にする。
+    }
+    public void SelectDeck(int i)
+    {
+        PlayerConfig.Deck = decks[i].cardIDList;//自分のデッキに選択したデッキを登録
+        foreach (var item in ButtonImage)
+        {
+            item.color = Color.white;
+        }
+        ButtonImage[i].color = Color.gray;//選択中のデッキのボタンの色を灰色にする。
+    }
+}
